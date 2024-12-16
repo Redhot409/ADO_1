97f59eb96832bf69d75642f783f2a6916a6a7734
Alexander Melikhov <alexandersermel@gmail.com>
2024-12-16 19:34:41 +0300

    SQL_Query_1_books_Count

--- ADO_1/Program.cs
+++ ADO_1/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using System.Dynamic;
 
 namespace ADO_1
 {
@@ -22,18 +23,18 @@ namespace ADO_1
             connection.Open();
 
             SqlDataReader reader=command.ExecuteReader();
-            const int padding = 20;
+            const int padding = 30;
             for (int i = 0; i < reader.FieldCount; i++)
             {
                 Console.Write(reader.GetName(i).PadRight(padding));
             
             }
 
-
             Console.WriteLine();
 
 
-            if (!reader.IsClosed)
+            //if (!reader.IsClosed)
+            if(reader.HasRows)
             {
                 while (reader.Read())
                 { 
@@ -48,36 +49,83 @@ namespace ADO_1
             reader.Close();
             connection.Close();
             Console.WriteLine("\n------------------------------------------------------------------------\n");
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            
 
-            SqlConnection connection_1 = new SqlConnection(connectionString);
-            string cmd_1 = "SELECT * FROM Books";
-            SqlCommand command_1 = new SqlCommand(cmd_1, connection_1);
-            connection_1.Open();
+            //SqlConnection connection_1 = new SqlConnection(connectionString);
+            //string cmd_1 = "SELECT * FROM Books";
+            //SqlCommand command_1 = new SqlCommand(cmd_1, connection_1);
+            //connection_1.Open();
 
-            SqlDataReader reader_1 = command_1.ExecuteReader();
-            const int padding_1 = 28;
-            for (int i = 0; i < reader_1.FieldCount; i++)
+            //SqlDataReader reader_1 = command_1.ExecuteReader();
+            //const int padding_1 = 28;
+            //for (int i = 0; i < reader_1.FieldCount; i++)
+            //{
+            //    Console.Write(reader_1.GetName(i).PadRight(padding_1));
+            //}
+            //    Console.WriteLine();
+
+
+            //if (!reader_1.IsClosed)
+            //{
+            //    while (reader_1.Read())
+            //    {
+            //        for (int i = 0; i < reader_1.FieldCount; i++)
+            //        {
+            //            Console.Write(reader_1[i].ToString().PadRight(padding_1));
+            //        }
+            //        Console.WriteLine();
+            //    }
+            //}
+
+            //reader_1.Close();
+            //connection_1.Close();
+            //Console.WriteLine("\n-----------------------------------------------------------------------\n");
+
+            command.CommandText = "SELECT book_title, last_name + ' ' + first_name AS 'Author'FROM Books JOIN Authors ON(author=author_id)";
+            connection.Open();
+            reader= command.ExecuteReader();
+            if (reader.HasRows)
             {
-                Console.Write(reader_1.GetName(i).PadRight(padding_1));
-            }
+                for (int i = 0; i < reader.FieldCount; i++)
+                    Console.Write(reader.GetName(i).PadRight(padding));
                 Console.WriteLine();
-
-
-            if (!reader_1.IsClosed)
-            {
-                while (reader_1.Read())
+                while (reader.Read())
                 {
-                    for (int i = 0; i < reader_1.FieldCount; i++)
+                    for (int i = 0; i < reader.FieldCount; i++)
                     {
-                        Console.Write(reader_1[i].ToString().PadRight(padding_1));
+                        Console.Write(reader[i].ToString().PadRight(padding));
+                    }
+                    Console.WriteLine();
+                
+                }
+                       
+            }
+            reader.Close();
+            connection.Close();
+            Console.WriteLine("\n=====================================================\n");
+            //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            command.CommandText = "SELECT first_name+' '+last_name AS 'Author', COUNT(book_id) AS 'Books count'" +
+                                  "FROM Books JOIN Authors ON (author=author_id)" +
+                                  "GROUP BY first_name, last_name";
+            connection.Open();
+            reader= command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                    Console.Write(reader.GetName(i).PadRight(padding));
+                Console.WriteLine();
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        Console.Write(reader[i].ToString().PadRight(padding));
                     }
                     Console.WriteLine();
                 }
             }
-
-            reader_1.Close();
-            connection_1.Close();
-            Console.WriteLine("\n-----------------------------------------------------------------------\n");
+            connection.Close();
 
         }
     }
