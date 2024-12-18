23522859ac21fd73c84cf9cc98a782f53fadd65f
Alexander Melikhov <alexandersermel@gmail.com>
2024-12-18 18:17:22 +0300

    Count books_done

---
+++ Library2/Library.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Forms;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Library2
+{
+    internal class Library
+    {
+        public static string connectionString = "";
+        static  SqlConnection connection = null;
+        static readonly string delimeter = "\n--------------------------------\n";
+        static Library()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["Library"].ConnectionString;
+            connection=new SqlConnection(connectionString);
+            Console.WriteLine(connectionString);
+        }
+        public static void Select(string fields, string tables,string condition="",int padding=20)
+        {
+            string cmd = $"SELECT {fields} FROM {tables}";
+            if (condition.Length > 0) cmd += $"WHERE {condition}";
+            cmd += ";";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                Console.WriteLine(delimeter);
+                for (int i = 0; i < reader.FieldCount; i++)
+                    Console.Write(reader.GetName(i).PadRight(padding));
+                Console.WriteLine();
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        Console.Write(reader[i].ToString().PadRight(padding));
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine(delimeter);
+                }
+            }
+
+            reader.Close();
+            connection.Close();
+            
+        }
+        public static void Insert(string table, string fields, string values)
+        {
+            string cmd = $"INSERT {table}({fields}) VALUES ({values});";
+            SqlCommand command = new SqlCommand (cmd, connection);
+            connection.Open ();
+            command.ExecuteNonQuery();
+            connection.Close();
+            }
+    }
+}
---
+++ Library2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Library2
+{
+    
+     class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Library.connectionString);
+            
+
+            Library.Insert("Authors", "author_id,last_name,first_name", "12,'Mazin','Alexander'");
+           // Library.Insert("Authors", "author_id,last_name,first_name", "5,'Bjarne','Stroustrup'");
+            //Library.Insert
+               // (
+                //"Books",
+                //"book_id,title_book,book_size,publish_date,author",
+               // "6,'Vikings',950,'2010-10-10',10"
+               // );
+            Library.Select("author_id,first_name,last_name", "Authors");
+            Library.Select("book_title,size,[Author]=first_name+ ' ' + last_name","Books,Authors","author=author_id;DROP TABLE Genres",15);
+
+        }
+    }
+}
