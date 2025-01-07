954548bc5d32330d48c9b70659eeb21f5c189ab6
Alexander Melikhov <alexandersermel@gmail.com>
2025-01-07 19:29:00 +0300

    Parametrised queries in Library2 done

--- Library2/Library.cs
+++ Library2/Library.cs
@@ -56,15 +56,17 @@ namespace Library
             connection.Open ();
             command.ExecuteNonQuery();
             connection.Close();
-            }
+        }
+
+
         public static void InsertAuthor(int id, string last_name, string first_name)
         {
 
-            string cmd = "INSERT Authors(author_id,last_name,first_name VALUES (@id,@last_name,@first_name)";
+            string cmd = "INSERT Authors(author_id, last_name, first_name) VALUES (@id, @last_name, @first_name)";
             SqlCommand command = new SqlCommand(cmd, connection);
 
 
-           SqlParameter p_id = new SqlParameter("@id", SqlDbType.Int);  p_id.Value = id;
+            SqlParameter p_id = new SqlParameter("@id", SqlDbType.Int);  p_id.Value = id;
             SqlParameter p_last_name = new SqlParameter("@last_name", SqlDbType.NVarChar, 150); p_last_name.Value = last_name;
             SqlParameter p_first_name = new SqlParameter("@first_name", SqlDbType.NVarChar, 150); p_first_name.Value = first_name;
 
@@ -76,36 +78,38 @@ namespace Library
             command.ExecuteNonQuery();
             connection.Close ();
         }
+
+        public static void InsertBook(int id, string title, int size, string date, string author)
+        {
+            string cmd =
+                "INSERT Books(book_id, book_title,book_size, publish_date,author) " +
+                "VALUES (@id,@title,@size,@date,@author)";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            command.Parameters.Add("@id", SqlDbType.Int).Value = id;
+            command.Parameters.Add("@title", SqlDbType.NVarChar, 256).Value = title;
+            command.Parameters.Add("@size", SqlDbType.Int).Value = size;
+            command.Parameters.Add("@date", SqlDbType.Date).Value = date;
+            command.Parameters.Add("@author", SqlDbType.Int).Value = GetAuthorID(author);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public static int GetAuthorID(string full_name)
+        {
+
+            string cmd =
+                "SELECT author_id FROM Authors WHERE last_name = @last_name AND first_name=@first_name";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            command.Parameters.Add("@first_name", SqlDbType.NVarChar, 150).Value = full_name.Split(' ').First();
+            command.Parameters.Add("@last_name", SqlDbType.NVarChar, 150).Value = full_name.Split(' ').Last();
+
+            connection.Open();
+            int id = Convert.ToInt32 (command.ExecuteScalar());
+            connection.Close();
+            return id;
+        }
     }
-    //public static void InsertBook(int id, string title, int size, string date, string author)
-    //{
-    //    string cmd =
-    //        "INSERT Books(book_id, book_title,book_size, publish_date,author) " +
-    //        "VALUES (@id,@title,@size,@date,@author)";
-    //    SqlCommand command = new SqlCommand(cmd, connection);
-    //    command.Parameters.Add("@id", SqlDbType.Int).Value = id;
-    //    command.Parameters.Add("@title", SqlDbType.NVarChar,256).Value = title;
-    //    command.Parameters.Add("@size", SqlDbType.Int).Value = size;
-    //    command.Parameters.Add("@date", SqlDbType.Date).Value = date;
-    //    command.Parameters.Add("@author", SqlDbType.Int).Value = GetAuthorID(author);
 
-    //    connection.Open();
-    //    command.ExecuteNonQuery();
-    //    connection.Close();
-    //}
-
-    //public static int GetAuthorID(string full_name)
-    //{
-
-    //    string cmd =
-    //        "SELECT author_id FROM Authors WHERE last_name = @last_name AND first_name=@first_name";
-    //    SqlCommand command = new SqlCommand(cmd, connection);
-    //    command.Parameters.Add("@first_name", SqlDbType.NVarChar, 150).Value = full_name.Split(' ').First();
-    //    command.Parameters.Add("@last_name", SqlDbType.NVarChar, 150).Value = full_name.Split(' ').Last();
-
-    //    connection.Open();
-    //    int id = Convert.ToInt32 (command.ExecuteScalar());
-    //    connection.Close();
-    //    return id;
-    //}
 }
--- Library2/Program.cs
+++ Library2/Program.cs
@@ -15,19 +15,18 @@ namespace Library
        
          static void Main(string[] args)
          {
-          
-            //Console.WriteLine(Library.GetAuthorID("Bjarne Stroustrup"));Data Source=(localdb)\MSSQLLocalDB;Initial
-            //Catalog=Library;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False
-           // Library.InsertBook(7, "C++ Programming Language", 331, "1986-01-29", "Bjarne Stroustrup");
-           // Library.InsertAuthor(5, "Stroustrup", "Bjarne");
-            Library.Insert("Authors", "author_id,last_name,first_name", "16,'Mazinsss','Alexander'");
+
+            Console.WriteLine(Library.GetAuthorID("Alexander Mazin"));
+            Library.InsertBook(19, "C++ Programming Language", 331, "1986-01-29", "Stroustrup Bjarne");
+            //Library.InsertAuthor(23, "Gadge", "Randy");
+           // Library.Insert("Authors", "author_id,last_name,first_name", "22,'Mazinsss','Alexander'");
            // Library.Insert("Authors", "author_id,last_name,first_name", "5,'Bjarne','Stroustrup'");
-            Library.Insert
-                (
-                "Books",
-                "book_id,book_title,book_size,publish_date,author",
-                "11,'Vikings',950,'2010-10-10',10"
-                );
+           // Library.Insert
+               // (
+               // "Books",
+               // "book_id,book_title,book_size,publish_date,author",
+              //  "16,'Vikings',950,'2010-10-10',10"
+               // );
             Library.Select("author_id,first_name,last_name", "Authors");
             Library.Select("book_title,publish_date,[Author]=first_name+ ' ' + last_name","Books,Authors","author=author_id",32);
 
